e2a5a62d316ac36017c2df4e08f80ef6e3e610e8
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-12-30 13:39:13 -0500

    Add custom signature description

--- R/md.human.custom.R
+++ R/md.human.custom.R
@@ -1,5 +1,11 @@
-#' md.human in a custom data frame (human)
+#' md.human.custom in a custom data frame (human)
 #'
-#' md.human in a custom data frame (human)
+#' md.human.custom in a custom data frame (human)
 #'
+#' @param cancer cancer-related gene signatures
+#' @param cancer/PAM50 50-gene classifier for four breast cancer types. Source,
+#' zip file with data, analysis and results files: \url{https://genome.unc.edu/pubsup/breastGEO/PAM50.zip}
+#' @param pharmacology drug metabolism-related gene signatures
+#' @param pharmacology/ADME* core, extended and related drug-metabolizing genes.
+#' Source: \url{http://pharmaadme.org/}
 "md.human.custom"
